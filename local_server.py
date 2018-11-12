d93419bbe1feaf0b459d5042ec240a54a51f2275
DDOU <ddou@connect.ust.hk>
2018-11-11 20:48:14 -0600

    response message

--- local_server.py
+++ local_server.py
@@ -2,7 +2,8 @@ import socket
 import sys
 from _thread import *
 
-def new_client(c):
+def new_client(c, server_id, DNS_map):
+	print(DNS_map)
 
 	while True:
 		try:
@@ -13,10 +14,25 @@ def new_client(c):
 
 			log_file = open(server_id+'.log','a+')
 			print(output)
-			log_file.write(output+'\n\n')
+			log_file.write(output+'\n')
 			log_file.close()
 
-			c.send('Result'.encode("utf-8"))
+			try:
+				client_id = output.split(",")[0]
+				hostname = output.split(",")[1]
+				dns_type = output.split(",")[2]
+				if dns_type != 'I' and dns_type != 'R':
+					raise e
+
+				if DNS_map.__contains__(hostname):
+					c.send(('0x00, '+server_id+', '+DNS_map[hostname]).encode("utf-8"))
+				else:
+					c.send(('0xFF, '+client_id+', Host not found').encode("utf-8"))
+
+			except Exception as e:
+				print("Invalid format")
+				c.send(('0xEE, '+client_id+', Invalid format').encode("utf-8"))
+
 
 		except KeyboardInterrupt:
 			print()
@@ -31,6 +47,18 @@ def new_client(c):
 
 
 def local_server(server_id, server_port, mapping_files, servers_list):
+	
+	# Read all the lines into a dict
+	DNS_map = {}
+	f = open(mapping_files, "r")
+	for line in f:
+		line = line.strip()
+		domain = line.split(" ")[0]
+		ip = line.split(" ")[1]
+		DNS_map[domain] = ip
+
+	f.close()
+
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 	host = '0.0.0.0'	#listen on this pc
 	port = int(server_port)
@@ -47,7 +75,7 @@ def local_server(server_id, server_port, mapping_files, servers_list):
 		print('Got connection from', addr)
 		c.send('Connection created. Welcome!'.encode("utf-8"))
 
-		start_new_thread(new_client, (c,)) 
+		start_new_thread(new_client, (c, server_id, DNS_map)) 
 
 
 if __name__ == '__main__':
